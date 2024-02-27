4983029e04ea795774c61df7f28823bced847291
mayur2004Id <gupta.mayur@kalvium.community>
2024-02-27 11:57:25 +0530

    yes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
-function Carousel() {
-}
+const Carousel = () => {
+    const [imgCount, setImgCount] = useState(0);
+
+    const rightButton = () => {
+        if (imgCount < 2) {
+            setImgCount(imgCount + 1);
+        } else {
+            setImgCount(0);
+        }
+    };
+
+    const leftButton = () => {
+        if (imgCount > 0) {
+            setImgCount(imgCount - 1);
+        } else {
+            setImgCount(2);
+        }
+    };
+    
+    return (
+        <>
+            <div className="kkk">
+                <div className="rightButton" onClick={rightButton} style={{ right: 0 }}><ArrowBackIosIcon /></div>
+            </div>
+            <div className="body">
+                <img src={images[imgCount].img} alt="" className="img" />
+                <h1 className="title">{images[imgCount].title}</h1>
+                <h2 className="subtitle">{images[imgCount].subtitle}</h2>
+                <div className="leftButton" onClick={leftButton}><ArrowForwardIosIcon /></div>
+            </div>
+        </>
+    );
+};
 
 export default Carousel;
